89ac93bf53aac77f53f4a4ceb6e0990ec530c166
MahanthiSriAsish1 <110473671+MahanthiSriAsish1@users.noreply.github.com>
2024-09-20 14:52:20 +0530

    Initial commit for Maven project

---
+++ src/main/java/com/deloitte/demo/Employee.java
@@ -0,0 +1,59 @@
+package com.deloitte.demo;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "emp")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private int id;
+
+    @Column(name = "emp_name")
+    private String name;
+
+    @Column(name = "emp_salary")
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, double salary) {
+    	this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+    }
+}
